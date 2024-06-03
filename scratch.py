0359d752a7473112417ff5d019b8585468f3ce6c
ewnike <wpltd5@yahoo.com>
2024-06-02 23:28:03 -0500

    add initial files for project

---
+++ scratch.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f7bddbef41b9c1f4cdafc5e7a3b8e184cd044f5ac9d676d60c517418ddb1ec8
+size 16
---
+++ venv/Scripts/activate.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c6c5376d044f6a3e0e334a24d60df2d7f91dc4bde38019059f8feac6aa10566e
+size 975
