82d5e1abeb24aa58e095df53537b21e700d85373
ewnike <wpltd5@yahoo.com>
2024-06-05 13:00:05 -0500

    adding data tables to repo

---
+++ game_goalie_stats.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e2e4dd1749216f1c240d7390bd9c0c06de12c383a52f465bd29b71bbfc450e00
+size 48188662
---
+++ game_goals.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0389eb66199fd44d6b58118b906e3e7f58776065145fb3efb94d04b8affad6e6
+size 34452026
