6bc13ee6aa398d646ab6d62a2c120fa7ab8de4ed
ewnike <wpltd5@yahoo.com>
2024-06-05 21:57:33 -0500

    removed sql files

--- game_scratches.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bcdb7d18cf14483e8a48854668428e832bddea1db90efdb2c0770bd60edae992
-size 12455912
--- player_info.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:77beb587790b5a7e521d59c547a96e37160ef8c48b0694016da8cce6b3f13795
-size 1114092
