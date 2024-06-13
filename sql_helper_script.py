c2931cb2734226f8227aba54354989964347e720
ewnike <wpltd5@yahoo.com>
2024-06-13 14:12:07 -0500

    added helper file for sql connection and to get data from MADS_NHL db.

---
+++ sql_helper_script.py
@@ -0,0 +1,55 @@
+#This is a helper function to connect to the MADS_NHL database
+#It uses load_dotenv() and takes advantage of the python .env so no private information is displayed when using.
+
+import pandas as pd
+import os
+import sqlalchemy
+from sqlalchemy import create_engine, text, inspect
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+print(f"Using port: {PORT}")
+# Database connection details
+DATABASE_TYPE = os.getenv('DATABASE_TYPE')
+DBAPI = os.getenv('DBAPI')
+ENDPOINT = os.getenv('ENDPOINT')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+PORT = int(os.getenv('PORT'))  # Provide default value if not set
+
+DATABASE = os.getenv('DATABASE')
+
+connection_string = f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}"
+#print(connection_string)
+engine = create_engine(connection_string)
+
+Session = sessionmaker(bind=engine)
+
+# Create a session
+session = Session()
+
+#Enter your query here:
+    
+query = 'SELECT * FROM "Corsi_20152016" LIMIT 5;'
+
+try:
+    # Read the SQL query into a DataFrame
+    df = pd.read_sql(query, engine)
+    
+    # Print the DataFrame
+    print(df)
+    
+    # Save the DataFrame to a CSV file without the index
+    df.to_csv('output.csv', index=False)
+
+except Exception as e:
+    print(f"Error occurred: {e}")
+
+finally:
+    engine.dispose()  # Close the engine
+    
