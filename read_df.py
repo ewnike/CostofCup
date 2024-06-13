c48ab734299d39d3938560adaed9488954fadb16
Jeffrey Vartabedian <vartabje@umich.edu>
2024-06-12 20:49:18 -0400

    Deleted extra files, ran ruff on files, modified paths, and updated requirements.txt

--- read_df.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("Corsi_LiteI.csv")
-print(df.head(50))
--- test_injct.py
+++ test_injct.py
@@ -1,3 +1,28 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:284e3d583f492dc64ff9e55a019d0c190afa67acd67255b296a06cc742d32ee0
-size 752
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from sqlalchemy import create_engine
+import datetime
+import psycopg2
+import sys
+import getpass
+import zipfile
+
+# Read CSV file into DataFrame
+df = pd.read_csv(r"kaggle_stats/team_info.csv")
+
+# Database connection details
+DATABASE_TYPE = "postgresql"
+DBAPI = "psycopg2"
+ENDPOINT = "localhost"
+USER = "postgres"
+PASSWORD = getpass.getpass(prompt="Password: ", stream=None)
+PORT = 5433  # default port for PostgreSQL
+DATABASE = "postgres"
+
+# Create the connection string
+connection_string = f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}"
+engine = create_engine(connection_string)
+
+# Insert data into PostgreSQL table
+df.to_sql("team_info", engine, if_exists="replace", index=False)
